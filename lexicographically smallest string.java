c25fe24c311ab1cb792fc8b397baddb9eb5cd1e0
Saloni Puraswani <salonipuraswani2004@gmail.com>
2024-09-11 14:41:01 +0530

    Update lexicographically smallest string.java

--- lexicographically smallest string.java
+++ lexicographically smallest string.java
@@ -1,35 +1,46 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
 class HelloWorld {
     public static void main(String[] args) {
         String s1="ABCA";
-        String s2="FFFT";
+        String s2="TFFT";
         System.out.println(ans(s1,s2));
     }
     public static String ans(String s1,String s2){
-        String s=new String();
-        int L1=s1.length();
-        int L2=s2.length();
-        int size=L1+L2-1;
-        char arr[]=new char[size];
-        for(int i=0;i<size;i++){
-            arr[i]='A';
-        }
+        int l1=s1.length();
+        int l2=s2.length();
+        int size=l1+l2-1;
         int flag=0;
-        for(int i=L2-1;i>=0;i--){
+        char arr[]=new char[size];
+        Arrays.fill(arr,'0');
+        for(int i=l2-1;i>=0;i--){
             if(s2.charAt(i)=='T'){
-                if(i+L1-1<size){
-                    flag=1;
-                    for(int j=0;j<L1;j++){
+                flag=1;
+                for(int j=0;j<l1;j++){
+                    if(arr[i+j]=='0'){
                         arr[i+j]=s1.charAt(j);
                     }
-                    break;
+                    else if(arr[i+j]==s1.charAt(j)){
+                        
+                    }
+                    else{
+                        return "-1";
+                    }
                 }
             }
         }
         if(flag==0){
             return "-1";
         }
-        for(int i=0;i<arr.length;i++){
-            s=s+arr[i];
+        String s="";
+        for(int i=0;i<size;i++){
+            if(arr[i]=='0'){
+                s=s+'A';
+            }
+            else{
+                s=s+arr[i];
+            }
         }
         return s;
     }
